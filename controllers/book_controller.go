0d6fe1050cad077f45d87cf460fcc967aee801b6
Leonardo Antunes <leonardoantunes1401@gmail.com>
2023-06-05 21:04:52 -0300

    add book update, delete and find by id

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -10,6 +10,9 @@ import (
 func BuildBookController(server *gin.Engine) {
 	server.GET("/api/v1/books", listBooks)
 	server.POST("/api/v1/books", createBook)
+	server.GET("/api/v1/books/:id", findBookById)
+	server.DELETE("/api/v1/books/:id", deleteBookById)
+	server.PUT("/api/v1/books/:id", updateBook)
 }
 
 func listBooks(context *gin.Context) {
@@ -39,4 +42,49 @@ func createBook(context *gin.Context) {
 type createBookInput struct {
 	Title string `json:"title" binding:"required"`
 	Author string `json:"author" binding:"required"`
+}
+
+func findBookById(context *gin.Context) {
+	var book models.Book
+
+	if err := models.Database.Where("id = ?", context.Param("id")).First(&book).Error; err != nil {
+		context.JSON(http.StatusNotFound, gin.H{ "error": "Entity not found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, book)
+}
+
+func deleteBookById(context *gin.Context) {
+	var book models.Book
+
+	if err := models.Database.Where("id = ?", context.Param("id")).First(&book).Error; err != nil {
+		context.JSON(http.StatusNotFound, gin.H{ "error": "Entity not found"})
+		return
+	}
+
+	models.Database.Delete(&book)
+
+	context.Status(http.StatusNoContent)
+}
+
+type updateBookInput struct {
+	Title string `json:"title"`
+	Author string `json:"author"`
+}
+
+func updateBook(context *gin.Context) {
+	var input updateBookInput
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H { "error": "Invalid Request", "details": err.Error() })
+		return
+	}
+
+	var book models.Book
+	if err := models.Database.Where("id = ?", context.Param("id")).First(&book).Error; err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": "entity not found"})
+	}
+
+	models.Database.Model(&book).Updates(input)
+	context.JSON(http.StatusOK, &book)
 }
